b960ab3d5bde59b62716a281b4ef5ae1b48ad0f1
Qiu <423822728@qq.com>
2016-04-12 17:19:55 +0800

    添加轮播图

--- fis-conf.js
+++ fis-conf.js
@@ -1,22 +1,40 @@
-/*
- * 用于开发用
- */
 
-//hb预览的配置
-fis.match('*.{js,css}',{
-	useHash:true
-})
-fis.match('_*.*',{
-	release:false
-})
 fis.match('::packager', 
     { postpackager: fis.plugin('loader', { allInOne: false })
 });
-fis.match('_*.css',{
-	packTo:'css/qiu-mobiui.css'
+fis.match('{_,jqz}*.css',{
+	packTo:'css/qiu-mobui.css'
 })
-fis.match('_*.js',{
-	packTo:'js/jqz-mobiui.js'
+fis.match('{_,jqz}*.js',{
+	packTo:'js/jqz-mobui.js'
+})
+
+fis.match("_.*",{
+	release:false
+})
+fis.match("*.html",{
+	release:false
+})//默认不产出
+/*release*/
+fis.match('./fonts/*',{
+	release:'./font/$0'
+})
+fis.match('jqz-mobui.js',{
+	release:'./jqz-mobui.js'
+})
+fis.match('qiu-mobui.css',{
+	release:'./qiu-mobui.css'
+})
+fis.match('jqz-mobui.js',{
+	optimizer: fis.plugin('uglify-js'),
+	release:'./jqz-mobui.min.js'
+})
+fis.match('qiu-mobui.css',{
+	optimizer: fis.plugin('clean-css'),
+	release:'./qiu-mobui.min.css'
+})
+fis.media('hb').match('*.{css,js}',{
+	useHash:true,
 })
 fis.media('hb').match('*.*',{
 	release:'/trunk/$0'
--- js/_page.js
+++ js/_page.js
@@ -1,5 +1,11 @@
 ;(function($){
+	/*下拉列表*/
 	$('.floder').on('click','.floder-t',function(){
 		$(this).parent('.floder').toggleClass('active');
 	})
+	/*后退*/
+	$('.back').on('click',function(){
+		history.go(-1);
+	})
+	
 })(window.Zepto||window.jQuery)
