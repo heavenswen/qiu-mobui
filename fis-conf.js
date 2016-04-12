d6742297cfdfd5086ad93803737fc3a3b548ccc3
Qiu <423822728@qq.com>
2016-04-12 17:52:26 +0800

    添加轮播图

--- fis-conf.js
+++ fis-conf.js
@@ -2,37 +2,20 @@
 fis.match('::packager', 
     { postpackager: fis.plugin('loader', { allInOne: false })
 });
-fis.match('{_,jqz}*.css',{
-	packTo:'css/qiu-mobui.css'
+fis.match('*.css',{
+	packTo:'css/qiu-mobui.css',
+	optimizer: fis.plugin('clean-css'),
+	release:'./css/qiu-mobui.min.css'
 })
-fis.match('{_,jqz}*.js',{
-	packTo:'js/jqz-mobui.js'
+fis.match('*.js',{
+	release:'js/qiu-mobui.js',
+	optimizer: fis.plugin('uglify-js'),
+	release:'./js/qiu-mobui.min.js'
+})
+fis.match('*.{less,html,md,png,jpg}',{
+	release:false
 })
 
-fis.match("_.*",{
-	release:false
-})
-fis.match("*.html",{
-	release:false
-})//默认不产出
-/*release*/
-fis.match('./fonts/*',{
-	release:'./font/$0'
-})
-fis.match('jqz-mobui.js',{
-	release:'./jqz-mobui.js'
-})
-fis.match('qiu-mobui.css',{
-	release:'./qiu-mobui.css'
-})
-fis.match('jqz-mobui.js',{
-	optimizer: fis.plugin('uglify-js'),
-	release:'./jqz-mobui.min.js'
-})
-fis.match('qiu-mobui.css',{
-	optimizer: fis.plugin('clean-css'),
-	release:'./qiu-mobui.min.css'
-})
 fis.media('hb').match('*.{css,js}',{
 	useHash:true,
 })
