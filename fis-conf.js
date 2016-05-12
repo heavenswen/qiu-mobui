388e151c55a21ad22d41355c9588e7a7d54e9eae
Qiu <423822728@qq.com>
2016-05-12 10:59:06 +0800

    修改了fis-conf的配置，兼容pc的下拉选项。

--- fis-conf.js
+++ fis-conf.js
@@ -1,52 +1,57 @@
 /*
  * 2016-04-15
  */
-var name ='qiu-mobui',
+var name = 'qiu-mobui',
 	css = '/css/*.css',
-	js = '/js/*.js';
-	
+	js = '/js/*.js',
+	re = false;
 
-fis.match('::packager', 
-    { postpackager: fis.plugin('loader', { allInOne: false })
+fis.match('::packager', {
+	postpackager: fis.plugin('loader', {
+		allInOne: true
+	})
 });
-fis.match('**', {
-  deploy: [
-    fis.plugin('skip-packed'),
 
-    fis.plugin('local-deliver', {
-      to: 'lib'
-    })
-  ]
-})
-fis.match('*.{less,md,png,gif,jpg,html,htm}',{
+fis.match('*.{less,md}', {
 	loaderLang: false,
-	release:false
-})
-fis.match('lib/*.*',{
-	loaderLang: false,
-	release:false
-})
-fis.match('fonts/*.{css,html}',{
-	loaderLang: false,
-	release:false
-})
-fis.match('fonts/*.{ttf,woff,svg,eot}',{
-	release:'$0',
-	url:'..$0'
-})
-fis.match(css,{
-	packTo:"./css/"+name+".css",
-	url:'.$0'
-})
-fis.match(js,{
-	packTo:'./js/'+name+'.js',
-	url:'.$0'
+	release: false
 })
 
-/*hb ide*/
-fis.media('hb').match('*.{css,js}',{
-	useHash:true,
+fis.match('fonts/*.{css,html}', {
+	release: false
 })
-fis.media('hb').match('*.*',{
-	release:'./trunk/$0'
+
+fis.match('fonts/*.{ttf,woff,svg,eot}', {
+	release: '$0',
+	url: '..$0'
+}) 
+fis.match('./dist/*', {
+	release: false
+})
+/*合并*/
+fis.match(css, {
+	packTo: "/css/" + name + ".css",
+})
+fis.match(js, {
+	packTo: '/js/' + name + '.js',
+})
+/*生成应用版*/
+fis.media('dist').match('**', {
+	deploy: [
+		fis.plugin('skip-packed'),
+		fis.plugin('local-deliver', {
+			to: './dist/'
+		})
+	]
+})
+fis.media('dist').match('*.{png,gif,jpg,html,htm}', {
+	release:false
+})
+fis.media('dist').match(js,{
+	 optimizer: fis.plugin('uglify-js'),
+	 release:'./js/'+name+'.min.js'
+})
+fis.media('dist').match(css,{
+	 optimizer: fis.plugin('clean-css'),
+	 release:'./css/'+name+'.min.css'
 })
